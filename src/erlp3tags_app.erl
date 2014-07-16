2f253af8f09ac6c91aa42b1357adf40d2a2dcd77
Akintayo Abiona Olusegun <akintayo.segun@gmail.com>
2014-07-16 13:19:34 +0100

    Initial Commit.
    Created new project by rebar.
    
    Signed-off-by: Akintayo Abiona Olusegun <akintayo.segun@gmail.com>

---
+++ src/erlp3tags_app.erl
@@ -0,0 +1,16 @@
+-module(erlp3tags_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    erlp3tags_sup:start_link().
+
+stop(_State) ->
+    ok.
