fe4ea1f465a66842f1aeb44254070d5b1dfbaf2d
Akintayo Abiona Olusegun <akintayo.segun@gmail.com>
2014-07-28 18:46:39 +0100

    Tests modified for added {size, Size} in the response of v22 reader
    Signed-off-by: Akintayo Abiona Olusegun <akintayo.segun@gmail.com>

--- tests/erlp3tags_tests.erl
+++ tests/erlp3tags_tests.erl
@@ -16,7 +16,7 @@
 
 test_read_header(FileHandle) ->
   Expected = {ok, [{version, {2, 3, 0}},
-    {flags, [{unsync, 0}, {extended, 0}, {experimental, 0}, {footer, 0}]},
+    {flags, [{unsync, 0}, {extended, 0}, {experimental, 0}]},
     {size, 35250}]},
   {ok, FirstTen} = file:pread(FileHandle, 0, 10),
   Actual = id3_tag_reader:read_v2_header(FirstTen),
@@ -44,15 +44,16 @@ test_text_header_to_atom() ->
 
 test_parse_v22_frame_bin() ->
   ActualBUF = v22_reader:parse_frame_bin(<<"BUF">>, 8, <<12, 13, 14, 1, 1, 2, 3, 4>>),
-  ExpectedBUF = {buf, [{buffer_size, 789774}, {embedded_info, true}, {next_flag_offset, 16909060}]},
+  ExpectedBUF = {buf, [{size, 8}, {buffer_size, 789774}, {embedded_info, true}, {next_flag_offset, 16909060}]},
   ActualBUF = ExpectedBUF,
 
   ActualCNT = v22_reader:parse_frame_bin(<<"CNT">>, 5, <<0, 1, 2, 3, 4>>),
-  ExpectedCNT = {cnt, 16909060},
+  ExpectedCNT = {cnt, [{size, 5}, {playcount, 16909060}]},
   ActualCNT = ExpectedCNT,
 
   ExpectedCOM = v22_reader:parse_frame_bin(<<"COM">>, 42, <<1, $e, $n, $g, "Short Comment", 0, "This is the actual comment", " ", 0>>),
   ActualCOM = {com, [
+    {size, 42},
     {language, "eng"},
     {short_description, "Short Comment"},
     {comment, "This is the actual comment"}
@@ -61,6 +62,7 @@ test_parse_v22_frame_bin() ->
 
   ActualCRA = v22_reader:parse_frame_bin(<<"CRA">>, 10, <<"http://mailto:segun@ratendate.com", 32, $a, $b, $c, 0, 0, 33, 0, 44, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10>>),
   ExpectedCRA = {cra, [
+    {size, 10},
     {owner_id, "http://mailto:segun@ratendate.com abc"},
     {preview_start, 33},
     {preview_length, 44},
@@ -70,6 +72,7 @@ test_parse_v22_frame_bin() ->
 
   ActualCRM = v22_reader:parse_frame_bin(<<"CRM">>, 10, <<"http://mailto:segun@ratendate.com", 0, "no reason", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10>>),
   ExpectedCRM = {crm, [
+    {size, 10},
     {owner_id, "http://mailto:segun@ratendate.com"},
     {content_explanation, "no reason"},
     {encrypted_data, <<1, 2, 3, 4, 5, 6, 7, 8, 9, 10>>}
@@ -78,6 +81,7 @@ test_parse_v22_frame_bin() ->
 
   ActualETC = v22_reader:parse_frame_bin(<<"ETC">>, 10, <<2, 16#A, 0, 0, 0, 33>>),
   ExpectedETC = {etc, [
+    {size, 10},
     {time_stamp_format, milliseconds},
     {event, etc_variation},
     {timestamp, 33}
@@ -86,6 +90,7 @@ test_parse_v22_frame_bin() ->
 
   ActualEQU = v22_reader:parse_frame_bin(<<"EQU">>, 10, <<16, 255, 128, 0, 4>>),
   ExpectedEQU = {equ, [
+    {size, 10},
     {adjustment_bits, 16},
     {inc_or_dec, increment},
     {frequency, 32640},
@@ -95,6 +100,7 @@ test_parse_v22_frame_bin() ->
 
   ActualGEO = v22_reader:parse_frame_bin(<<"GEO">>, 10, <<1, "text/xhtml", 0, "hello.xml", 0, "Hello World In XML", 0, 44, 55, 66, 77, 88, 99, 100, 33, 22, 11>>),
   ExpectedGEO = {geo, [
+    {size, 10},
     {encoding, 1},
     {mime_type, "text/xhtml"},
     {filename, "hello.xml"},
@@ -105,6 +111,7 @@ test_parse_v22_frame_bin() ->
 
   ActualIPL = v22_reader:parse_frame_bin(<<"IPL">>, 10, <<0, "Director", 0, "Moses Monday", 0, "Producer", 0, "Wyte Tag", 0, "Creative Director", 0, "Aaron", 0>>),
   ExpectedIPL = {ipl, [
+    {size, 10},
     {ip, {involvement, "Director"}, {involvee, "Moses Monday"}},
     {ip, {involvement, "Producer"}, {involvee, "Wyte Tag"}},
     {ip, {involvement, "Creative Director"}, {involvee, "Aaron"}}
@@ -113,6 +120,7 @@ test_parse_v22_frame_bin() ->
 
   ActualLNK = v22_reader:parse_frame_bin(<<"LNK">>, 10, <<"TOS", "http://nalyrics.com.ng", 0>>),
   ExpectedLNK = {lnk, [
+    {size, 10},
     {frame_identifier, "TOS"},
     {url, "http://nalyrics.com.ng"},
     {additional_id_data, []}
@@ -121,6 +129,7 @@ test_parse_v22_frame_bin() ->
 
   ActualLNK2 = v22_reader:parse_frame_bin(<<"LNK">>, 10, <<"TOS", "http://nalyrics.com.ng", 0, "olu ", 0, "is ", 0, "a ", 0, "boy", 0>>),
   ExpectedLNK2 = {lnk, [
+    {size, 10},
     {frame_identifier, "TOS"},
     {url, "http://nalyrics.com.ng"},
     {additional_id_data, ["olu", "is", "a", "boy"]}
@@ -129,6 +138,7 @@ test_parse_v22_frame_bin() ->
 
   ActualMLL = v22_reader:parse_frame_bin(<<"MLL">>, 10, <<2, 3, 1, 2, 3, 3, 2, 1, 2, 1>>),
   ExpectedMLL = {mll, [
+    {size, 10},
     {frames_between_reference, 515},
     {bytes_between_reference, 66051},
     {milliseconds_between_reference, 197121},
@@ -139,6 +149,7 @@ test_parse_v22_frame_bin() ->
 
   ActualPIC = v22_reader:parse_frame_bin(<<"PIC">>, 128, <<1, "PNG", 2, "Wande Coal In Allow Me To Kiss Your Hand", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10>>),
   ExpectedPIC = {pic, [
+    {size, 128},
     {encoding, 1},
     {image_format, "PNG"},
     {picture_type, 2},
@@ -149,6 +160,7 @@ test_parse_v22_frame_bin() ->
 
   ActualPOP = v22_reader:parse_frame_bin(<<"POP">>, 10, <<"akintayo.segun@gmail.com", 0, 254, 0, 1, 2, 3, 4>>),
   ExpectedPOP = {pop, [
+    {size, 10},
     {email_to_user, "akintayo.segun@gmail.com"},
     {rating, 254},
     {counter, 16909060}
@@ -157,6 +169,7 @@ test_parse_v22_frame_bin() ->
 
   ActualRVA = v22_reader:parse_frame_bin(<<"RVA">>, 10, <<16, 16, 0, 4, 0, 8, 0, 2, 0, 6>>),
   ExpectedRVA = {rva, [
+    {size, 10},
     {inc_or_dec_right, 1},
     {inc_or_dec_left, 0},
     {bits_used_for_volume, 16},
@@ -170,17 +183,18 @@ test_parse_v22_frame_bin() ->
   ActualSLT = v22_reader:parse_frame_bin(<<"SLT">>, 10, <<1, "ENG", 2, 1,
   "Strang", 0, 1, 0, "ers", 0, 1, 1, " in", 0, 2, 0, " the", 0, 2, 1, " night", 0, 2, 2, 0>>),
   ExpectedSLT = {slt, [
+    {size, 10},
     {encoding, 1},
     {language, "ENG"},
     {timestamp_format, utils:time_format_code_to_atom(2)},
     {content_type, utils:slt_content_type_code_to_atom(1)},
     {content_descriptor, [83, 116, 114, 97, 110, 103, 0, 1, 0, 101, 114, 115, 0, 1, 1, 32, 105, 110, 0, 2, 0, 32, 116, 104, 101, 0, 2, 1, 32, 110, 105, 103, 104, 116, 0, 2, 2]}
   ]},
-
   ActualSLT = ExpectedSLT,
 
   ActualTEN = v22_reader:parse_frame_bin(<<"TEN">>, 10, <<0, "Naija", 0>>),
   ExpectedTen = {ten, [
+    {size, 10},
     {encoding,0},
     {textstring,"Naija"}
   ]},
@@ -188,6 +202,7 @@ test_parse_v22_frame_bin() ->
 
   ActualUFI = v22_reader:parse_frame_bin(<<"UFI">>, 10, <<"The Don", 0, 1,2,3,4,5,6,7,8,9>>),
   ExpectedUFI = {ufi, [
+    {size, 10},
     {owner_identifier, "The Don"},
     {identifier, <<1,2,3,4,5,6,7,8,9>>}
   ]},
@@ -196,6 +211,7 @@ test_parse_v22_frame_bin() ->
   ActualULT = v22_reader:parse_frame_bin(<<"ULT">>, 10, <<0, "SPA", "This Is A Content Descriptor", 0, "This Is A Lyrics">>),
   %%io:format("RESULT: ~p~n", [ActualULT]),
   ExpectedULT = {ult, [
+    {size, 10},
     {encoding, 0},
     {language, "SPA"},
     {content_descriptor, "This Is A Content Descriptor"},
@@ -205,9 +221,11 @@ test_parse_v22_frame_bin() ->
 
   ActualWAF = v22_reader:parse_frame_bin(<<"WAF">>, 10, <<"http://nalyrics.com.ng">>),
   ExpectedWAF = {waf, [
+    {size, 10},
     {url, "http://nalyrics.com.ng"}
   ]},
   ActualWAF = ExpectedWAF,
+
   erlog:info("Testing v22_reader:parse_frame_bin/3 - passed~n").
 
 tests() ->
@@ -223,5 +241,5 @@ tests() ->
   test_text_header_to_atom(),
   erlog:info("~n---------------Tests Finished---------------~n"),
 
-  io:format("Result: ~p~n", [id3_tag_reader:read_tag(filename:join("misc", "mi_one_six_real.mp3"))]),
+  io:format("Result: ~p~n", [id3_tag_reader:read_tag(filename:join("misc", "ile_aye_azadus_ft_vector.mp3"))]),
   ok.
